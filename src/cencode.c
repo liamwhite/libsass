0dfbda33b5260b9d2a0f165701140cccd4ad7ea8
Liam P. White <byteslice@airmail.cc>
2019-02-20 15:45:03 -0500

    C -> C++

--- src/cencode.c
+++ src/cencode.c
@@ -5,7 +5,7 @@ This is part of the libb64 project, and has been placed in the public domain.
 For details, see http://sourceforge.net/projects/libb64
 */
 
-#include "b64/cencode.h"
+namespace base64 {
 
 void base64_init_encodestate(base64_encodestate* state_in)
 {
@@ -106,3 +106,4 @@ int base64_encode_blockend(char* code_out, base64_encodestate* state_in)
 	return (int)(codechar - code_out);
 }
 
+}
